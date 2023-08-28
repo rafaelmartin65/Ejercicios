64c8c52abcd05fa3031905bdd62b01b0d2e92165
rafaelmartin65 <rmartin65@gmail.com>
2023-08-28 20:23:01 +0100

    Arrays with integer

---
+++ src/com/cga/cliente/Enteros.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.cga.cliente;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author Alumno
+ *
+ */
+public class Enteros {
+
+	/**
+	 * @param args
+	 */
+	
+	static ArrayList<Enteros> enteros = new ArrayList<Enteros>();
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		leerNumerosEnteros ();
+	}
+
+	private static void leerNumerosEnteros() {
+		// TODO Auto-generated method stub
+		System.out.println("Introduce un numero entero");
+		int entero = sc.nextInt();
+		
+		while (entero != 0) {
+			
+			int suma =+ entero;
+			int cont =+ 1 ;
+			
+		}
+	}
+	
+	
+	
+
+}
